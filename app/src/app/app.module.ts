5f75d14d72f85ecb5615e9a793c377c9d0c22478
zerolive <zerolive@gmail.com>
2022-05-05 19:18:43 +0200

    Add DraftComponent with a placeholder

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
+import { DraftComponent } from './draft/draft.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    DraftComponent
   ],
   imports: [
     HttpClientModule,
---
+++ app/src/app/draft/draft.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DraftComponent } from './draft.component';
+
+describe('DraftComponent', () => {
+  let component: DraftComponent;
+  let fixture: ComponentFixture<DraftComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DraftComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DraftComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('has a placeholder', () => {
+
+    const placeholder = component.placeholder
+
+    expect(placeholder).toBe('Escribe aquí tu nota')
+  })
+});
