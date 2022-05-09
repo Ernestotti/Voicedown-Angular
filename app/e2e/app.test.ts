07b00fcb5a7a2681b7acbbb774863c50ea238c01
Ana Villamil <escritoriorevuelto@gmail.com>
2022-05-09 15:48:09 +0200

    remove legacy test

--- app/e2e/app.test.ts
+++ app/e2e/app.test.ts
@@ -1,16 +1,17 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core'
 import {render, screen, fireEvent} from '@testing-library/angular'
 import { AppComponent } from '../src/app/app.component'
-import { TableComponent } from '../src/app/table/table.component'
 import { HttpClientModule } from '@angular/common/http'
 
 describe('Testing library', () => {
     beforeEach( async() => {
         await render(AppComponent, {
             detectChanges: true,
-            declarations: [TableComponent],
             imports: [HttpClientModule],
             schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
         })
+    })
+    it("it's works", () => {
+        expect(true).toBeTruthy()
     })
 })
