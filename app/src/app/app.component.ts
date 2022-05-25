dbfed181a669151e1a69cee0efc1c4fdd8415500
Deby <debyta.mm@gmail.com>
2022-05-25 19:44:11 +0200

    Save notes from localStorage

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -14,7 +14,15 @@ export class AppComponent implements OnInit {
 
   constructor(private service: AppService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    let lista = localStorage.getItem('listNotes')
+    if(lista === null){
+      lista = JSON.stringify([])
+    }
+    let notesArray = JSON.parse(lista)
+    this.notes = notesArray 
+
+  }
 
   openDraft(): void {
     this.show = true
@@ -26,7 +34,9 @@ export class AppComponent implements OnInit {
   }
 
   getDraftContent(event:string): void {
-    this.notes.push(event)    
+    this.notes.push(event)
+    let notesString = JSON.stringify(this.notes)
+    localStorage.setItem('listNotes', notesString) 
     this.closeDraft()
   }
 }
