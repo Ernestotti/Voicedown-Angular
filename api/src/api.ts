6f0e7e2ee475a22cc753745917764db7ffd93a5c
Luciano Chirico <doglucho@MacBook-Air-de-Luciano.local>
2022-06-14 17:34:19 +0200

    the quotation marks were unified into single quotation marks

--- api/src/api.ts
+++ api/src/api.ts
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json({limit: '50mb'}))
 
 app.listen(3001, () => {
-    console.log(`server started at localhost:3001`)
+    console.log('server started at localhost:3001')
 })
 
 app.post('/saveNote', async (req: Request, res: Response) => {
