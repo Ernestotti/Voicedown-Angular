24db08e4d44a6ca2b13291751ec25fb51cfb4c8a
Rolope <rosalopezpenya@gmail.com>
2022-07-14 18:29:01 +0200

    Test the content of a note

--- app/src/app/note/note.component.spec.ts
+++ app/src/app/note/note.component.spec.ts
@@ -1,4 +1,6 @@
 import { NoteComponent } from './note.component';
+import {screen} from '@testing-library/angular'
+import { render } from '@testing-library/angular';
 
 describe('NoteComponent', () => {
   let component: NoteComponent;
@@ -8,4 +10,16 @@ describe('NoteComponent', () => {
     
     expect(component).toBeTruthy();
   });
+    
+  it('Should content a text the note', async () => {
+    const aTextNote = 'a text note'
+    await render(NoteComponent, {
+      componentProperties: {
+       note : aTextNote
+      }
+    })
+    const note = await screen.findByText(aTextNote)
+
+    expect(note.innerHTML).toContain(aTextNote)
+  })
 });
